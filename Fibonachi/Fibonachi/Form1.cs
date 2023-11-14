9e5f5a73df3e2a86a538d222bc4eb124d898c598
ZahraGholizade <150825723+ZahraGholizade@users.noreply.github.com>
2023-11-15 01:37:27 +0330

    Add files via upload

---
+++ Fibonachi/Fibonachi/Form1.cs
@@ -0,0 +1,40 @@
+﻿namespace Fibonachi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = int.Parse(textBox1.Text);
+                int x;
+                if (n <= 0)
+                {
+                    MessageBox.Show("اعداد کوچکتر از 1 وارد نکنید");
+                    return;
+                }
+                n = n - 1;
+                x = fib(n);
+                MessageBox.Show("جمله ی" + textBox1.Text + "ام برابر است با:" + x.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public int fib(int n)
+        {
+            if (n <= 0)
+                return n;
+            if (n == 1)
+                return n;
+            else
+                return fib(n - 1) + fib(n - 2);
+        }
+    }
+}
