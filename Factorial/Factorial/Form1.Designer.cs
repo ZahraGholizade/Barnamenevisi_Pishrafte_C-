e48e569e0df7b415ea90adaf06b1adcf9b909c4f
ZahraGholizade <150825723+ZahraGholizade@users.noreply.github.com>
2023-11-15 01:39:22 +0330

    Add files via upload

---
+++ Factorial/Factorial/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Factorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(257, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(299, 54);
+            label1.TabIndex = 0;
+            label1.Text = "محاسبه فاکتوریل";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.IndianRed;
+            button1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(293, 261);
+            button1.Name = "button1";
+            button1.Size = new Size(207, 71);
+            button1.TabIndex = 1;
+            button1.Text = "نمایش جواب";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ScrollBar;
+            textBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(168, 151);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(184, 43);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(368, 151);
+            label2.Name = "label2";
+            label2.Size = new Size(328, 41);
+            label2.TabIndex = 3;
+            label2.Text = "عدد مورد نظر را وارد کنید";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 358);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label2;
+    }
+}
---
+++ Factorial/Factorial/Form1.cs
@@ -0,0 +1,42 @@
+﻿namespace Factorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string n = textBox1.Text;
+                int i = int.Parse(n);
+                int f = fact(i);
+                MessageBox.Show("جواب" + textBox1.Text + "!برابر است با:" + f.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        public int fact(int i)
+        {
+            int f = 1;
+            for (int j = 1; j <= i; j++)
+            {
+                f *= j;
+            }
+            return f;
+        }
+
+    }
+
+}
